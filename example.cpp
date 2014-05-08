2ef12e42a758a41e205db353f096f9e1b52fc90e
Zac Bergquist <zbergquist99@gmail.com>
2014-05-08 16:07:29 -0400

    add code

---
+++ example.cpp
@@ -0,0 +1,18 @@
+#include "Hexdump.hpp"
+
+#include <iostream>
+
+int main()
+{
+    unsigned char data[150];
+    unsigned char c = 0;
+    for (auto& val : data)
+    {
+        val = c++;
+    }
+
+    std::cout << Hexdump(data, sizeof(data)) << std::endl;
+    std::cout << CustomHexdump<8, true>(data, sizeof(data)) << std::endl;
+    std::cout << CustomHexdump<32, false>(data, sizeof(data)) << std::endl;
+
+}
